7921ca2c8d2b70b3ee2967a6c59dc0c9f10d2e5d
Alexandr Sargsyan <sargsyan_alexandr@bk.ru>
2022-03-06 00:33:50 +0400

    add 1 home work shabatva or

---
+++ cpp/day2/1_weekName.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+
+int main (){
+
+int num;
+cout << "input num  \n ";
+cin >> num;
+bool a = true;
+
+	while (a) {
+
+
+		if (num >= 1 && num <= 7) {
+            	 a = false ;
+	}       
+        	else  {
+		 cout << "greq [1,7] mijakayqic tiv \n";
+			cin >> num;
+		}	
+	}
+	
+
+switch (num) {
+	
+		case 1:
+	              cout << " erkushabti \n";
+		      break;
+		case 2:
+                      cout << " erekshabti  \n";
+                      break;
+		case 3:
+                      cout << " chorekshabto \n";
+                      break;	
+		case 4:
+                      cout << " hingshabti  \n";
+                      break;
+		case 5:
+                      cout << " urbat \n";
+                      break;
+		case 6:
+                      cout << " shabat \n";
+                      break;
+		case 7:
+                      cout << " kiraki  \n";
+                      break;
+		default: 
+		      cout << "greq [1,7] mijakayqic tiv \n";
+			cin >> num;
+		} 
+
+        
+
+	return 0;
+}
