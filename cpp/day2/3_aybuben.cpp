b42163f436606189591a441bf15a5c97037e2b02
Alexandr Sargsyan <sargsyan_alexandr@bk.ru>
2022-03-06 16:14:06 +0400

    add 3 home work aybuben

---
+++ cpp/day2/3_aybuben.cpp
@@ -0,0 +1,25 @@
+#include  <iostream>
+using namespace std;
+
+int main (){
+char i;
+cout << "greq 1 tar \n";
+cin >> i; 
+
+	switch (i){
+
+		case 'a': case 'u': case 'e': case 'o': case 'y': case 'i':
+		case 'A': case 'U': case 'E': case 'O': case 'Y': case 'I':
+		cout << " dzaynavor e  \n";
+		break;
+
+		default :
+		cout << " baxadzayn e ";
+	} 
+
+
+
+
+
+return 0;
+}
