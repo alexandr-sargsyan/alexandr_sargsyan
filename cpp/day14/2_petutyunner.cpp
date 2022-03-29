860ab9ee798679d710ab405af80e4b7018bd5071
Alexandr Sargsyan <sargsyan_alexandr@bk.ru>
2022-03-29 05:25:12 +0400

    add home work day14

---
+++ cpp/day14/2_petutyunner.cpp
@@ -0,0 +1,184 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+using namespace std;
+
+
+
+struct info {
+    string countryName;
+    string capital;
+    string domain;
+    int population;
+    int area;
+    int gdp;
+    int phonecode;
+};
+enum enumf {
+    Armenia = 374,
+    Russia = 7,
+    France = 33,
+    Germany = 49,
+    Georgia = 995
+};
+int main(int argc , char * argv[]){
+
+  ifstream count ("countries.txt");
+  string a;
+  int rows = 0;
+
+	while (getline(count,a)){
+
+	   rows++;           
+
+	}
+   count.close();
+
+                                  // zangvaci chapnum voroshal @st toxeri qanaki
+   struct info arr[rows];
+ 
+  
+
+
+  enum enumf arrenum[] = {Armenia,Russia,France ,Germany,Georgia};
+  ifstream file ("countries.txt");
+  string b = "";
+  string strname  = "";
+  int j = 0;
+  int k = 0;
+  string parametrer[6];     
+  
+	while (!file.eof()){
+
+	file >> b;
+	parametrer[j] = b;
+	j++;
+           
+	   if (j == 6) {
+
+               arr[k].countryName = parametrer[0];
+               arr[k].capital = parametrer[1];
+               arr[k].domain = parametrer[2];
+               arr[k].population = stoi(parametrer[3]);
+               arr[k].area = stoi(parametrer[4]);
+               arr[k].gdp = stoi(parametrer[5]);
+               arr[k].phonecode = arrenum[k];
+            
+	       j = 0;
+               k = k + 1;
+           }
+        
+
+	} 
+
+	file.close();                           // zangvacy arjevoralnq amen@ ira hamapatasxan
+     
+
+ 
+
+
+
+	if (strcmp(argv[1], "search") == 0){
+
+	cout << "Input search mode (countryName / capital / domain / ) --->  ";
+
+	string m;
+        getline(cin,m);
+	
+
+	cout << endl << "Input " << m << " ---> ";
+	string n ;
+	getline(cin,n);	
+		
+		
+	int index;
+
+	if(m == "countryName") {	
+		for(int i = 0; i < rows; i++) {
+
+		   if (arr[i].countryName == n ) {
+			index = i; 
+			break;
+			}
+	
+		}
+
+	}
+
+
+	else if(m == "capital") {	
+		for(int i = 0; i < rows; i++) {
+
+		   if (arr[i].capital == n ) {
+			index = i; 
+			break;
+			}
+	
+		}
+
+	}
+
+
+	else if(m == "domain") {	
+		for(int i = 0; i < rows; i++) {
+
+		   if (arr[i].domain == n ) {
+			index = i; 
+			break;
+			}
+	
+		}
+
+	}
+
+	
+
+	for(int i = 0; i < 1; i++ ) {
+
+	
+    	cout << arr[index].countryName << endl;
+     	cout << arr[index].capital << endl;
+     	cout << arr[index].domain << endl;
+     	cout << arr[index].population << endl;
+     	cout << arr[index].area << endl;
+     	cout << arr[index].gdp << endl;
+    	cout << arr[index].phonecode << endl;
+	
+	}
+
+
+	}
+
+
+							// esel search yneli hetiya 
+
+
+
+
+
+
+
+
+
+
+
+
+
+////// hamemately hscralchum sor kanim 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
