f986d437e1fa48b60c40770c623c94a8b71e6f86
Alexandr Sargsyan <sargsyan_alexandr@bk.ru>
2022-03-30 04:10:10 +0400

    add home worc 10 Qaxaqnery prceaca lriv

--- cpp/day10/2_qaxaqner.cpp
+++ cpp/day10/2_qaxaqner.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 #include <ctime>                                   
 #include <string>                                 
-using namespace std;                               // halyam prcacchi bayc 
+using namespace std;                              
 
  int menuyHraman (void);
  void kanonner() ;
@@ -12,9 +12,9 @@ using namespace std;                               // halyam prcacchi bayc
 
 int menu(void) {  
  
-   cout<<"_____________________\n || QAXAQNER ||\n_____________________\n";
-   cout<<"Menu: \n 1. Sksem es ; \n 2. Sksi kompyutery ;\n " ;
-   cout<<" Yntreq tarberak : --->  ";
+   cout<<"_____________________\nCountries\n_____________________\n";
+   cout<<"Menu: \n 1. Me \n 2.Computer\n " ;
+   cout<<"Choose: " ;
    int c;
    cin >> c;
    return c;
@@ -46,67 +46,94 @@ int menu(void) {
 
   int x = a;
 
-  string arr [] = {"erevan" , "stepanakert", "masvkva" , "paris" , "oslo" , "astana" , "ankara" , "baku", "amsterdam" };
+  string arr [] = {"stepanakert","yerevan","tokio","paris", "moscow","new york", "baku","seul","tbilisi","ankara","amsterdam","bern","antananarivu","cambera","oslo","ottava","delli","copelhagen","phenian","nairobi","ulan baator","shushi","askeran","hadrut","qarvajar","bangladesh","aljir","lisabon","madrid","washington","london","don-i-rostov","sankt-peterburg","ufa","xarkov","mariupl" ,"karmir-shuka", "taxavard" , "majkalaashen","tox","gishi","xnapat" , "sarushen", "jartar", "xunushinak", "martunei", "martakert" , "ningi", "siptak-shen" , "gandzasahr","erasx","ekaterenburg"};
 
- int b = rand() % 6;
+ int b = rand() % 15;
  string mard ;
  bool c = false;       // es kiraca ver  2 rd ific skse 3 rord if- @ mtnevech
  bool f = true;
+
+
+ string ogt [100];
+ int w = 0;
 
 	  while (f) {
             
 		
 	  if (x == 2) {
 		
-		cout << arr[b] << " ---> ";
+		cout << arr[b];
 		x = 1;
 		c = true;
 		}
 
 	  if (x == 1) {
 		
-		cout << " input --->  ";
+		cout << "  --->  ";
 		cin >> mard;
+			
+		   for(int i = 0;i < 100;i++) {
+
+			if(mard == ogt[i]) {
+			
+			cout << " >>>>>>>>>>>>    OGTAGORCAVAC QAXAQ <<<<<<<<<<<< " << endl << " >>>>>>>>>>>>     GAME OVER    <<<<<<<<<<<<<<< " << endl;
+				return ;
+			}
+	
+		   }
+			
+		ogt[w] = mard;
+		w++;
 		x = 2;
 		}
 
 	  if ( c && arr[b][arr[b].length() - 1] != mard[0] ) {
 
-		cout << " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>|||||||     GAME OVER     ||||||||<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< " << endl << endl;
+		cout << " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>     GAME OVER    <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< " << endl << endl;
                      break;
 		}
 
 	if (c){
 	
-	arr[b] = "0000";
-	};
+	arr[b] = "";
+	}
 	  
       
 	for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
 
 	if(arr[i][0] == mard[mard.length() - 1] ){
 		
+		bool t = true ;
 		
-                b = i;
-		break;
+	   for (int j = 0; j < 100; j++) {
+		
+		if(arr[i] == ogt[j] ) {
+	
+			arr[i] = "";               // es stuguma ogtagorcvaca ta che eyd bary
+			i = 0;
+			t = false;
+		   }
+		}
+
+	  if(t){	
+	     b = i;
+	     ogt[w] = arr[b];
+	     w++;			
+	     break;
+		}
 	}
+
+
 		if ( i == (sizeof(arr)/sizeof(arr[0]) - 1)) {
-		cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>  ||||||  Kompi barery prcala duq haxtel ek !!!  |||||  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl << endl;
+		cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>  There are no any countries in computer, You are WIN   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
                 f = false ; 
 		break;
                     }
-
 
 	}
   }
 
 }
-
-
-
-
-
-
 
 
 int main (){
